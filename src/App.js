8f4fc5ff41698c9a23a05eec58f7cc24bade832c
Jane Kagan <kagan009@umn.edu>
2022-01-30 22:28:59 -0600

    Added in help modal.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import { useState } from 'react';
 
 function App() {
     const [view, setView] = useState("villager"); 
+    const [viewModal, setModal] = useState(false);
     let pageView;
+
+    const openModal = () => {
+        if (viewModal) {
+            setModal(false);
+        } else {
+            setModal(true);
+        }
+    }
 
     // Decide if person or item view. 
     // TODO: base this on a button
@@ -18,9 +27,18 @@ function App() {
     return (
         <div className="App">
             <div className="App__header">
-                <div className="App__headerText">Stardew Gift Guide</div>
+                <div className="App__headerText">Stardew Gift Guide <button onClick={() => openModal()} className="App__helpButton">?</button></div>
             </div>
             {pageView}
+            {viewModal && 
+                <div className="App__modal">
+                    <span class="close" onClick={() => openModal()}>&times;</span>
+                    This guide is still a work in progress. 
+                    A known bug is duplicate items showing up (universal tastes AND villager tastes sometimes conflict). In this case, 
+                    the <span className="ItemList__item">Pink-Highlighted Item</span> takes prescedence. 
+                    Please submit other bugs or data corrections to me <a href="https://github.com/maycontainjane/stardew-gifting/issues">here!</a>
+                </div>
+            }
         </div>
     );
 }
